bfde8718a27ec184c083e154b7ae8137dcf29c0f
Dani <dhuber88@sunrise.ch>
2024-10-06 21:35:21 +0200

    create user, reset password

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['user' => $user], 201);
+    }
+
+    public function updatePassword(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'current_password' => 'required',
+            'new_password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = Auth::user();
+
+        // Überprüfe das aktuelle Passwort
+        if (!Hash::check($request->current_password, $user->password)) {
+            return response()->json(['error' => 'Das aktuelle Passwort ist falsch'], 401);
+        }
+
+        // Aktualisiere das Passwort
+        $user->password = Hash::make($request->new_password);
+        $user->save();
+
+        return response()->json(['message' => 'Passwort erfolgreich aktualisiert'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,18 +4,31 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 // Öffentliche Route für alle Posts (ohne Authentifizierung)
+
+//Posts
+
 Route::get('/index', [PostController::class, 'index']);  // Alle Posts anzeigen
 Route::get('/posts/user/{userId}', [PostController::class, 'getPostsByUser']);
 Route::get('/search', [PostController::class, 'search'])->name('search');
+
+//User
+
+Route::post('/register', [UserController::class, 'register']);
 
 
 
 
 // Geschützte Routen für CRUD-Operationen auf /posts
 Route::middleware('auth:sanctum')->group(function () {
+
+    //Posts
     Route::get('/myposts', [PostController::class, 'myPosts']);  // Posts des eingeloggten Benutzers
     Route::resource('posts', PostController::class);  // CRUD für Posts (Erstellen, Bearbeiten, Löschen)
     Route::get('/categories', [CategoryController::class, 'index']);
+
+    //User
+    Route::post('/user/reset', [UserController::class, 'updatePassword']);
 });
