e6b8ab2a364cc49185b668d9a90c3741f22f1a43
Dani <dhuber88@sunrise.ch>
2024-10-07 11:52:13 +0200

    User-modell erweitert

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,15 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class); 
+    }
+
+    public function comments()
+    {
+    return $this->hasMany(Comment::class);
+    }
+
 }
